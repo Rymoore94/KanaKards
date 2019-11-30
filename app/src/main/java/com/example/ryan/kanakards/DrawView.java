a4fdc6cfecdc7175c7b9493c8df0d3bd530e8638
Ryan <rymoore@nmu.edu>
2019-11-30 01:16:27 -0500

    Added custom DrawView View and implemented a canvas in the TraceViewer activity that can be painted on with touch from the user.

---
+++ app/src/main/java/com/example/ryan/kanakards/DrawView.java
@@ -0,0 +1,82 @@
+package com.example.ryan.kanakards;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawView extends View {
+
+    static Path path;
+    private Paint drawPaint, canvasPaint;
+    static int paintColor = Color.BLACK;
+    private  float STROKE_WIDTH = 30f;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
+    public DrawView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        path = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(STROKE_WIDTH);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(path, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        canvasPaint.setColor(paintColor);
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawCanvas.drawPath(path, drawPaint);
+                path.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                path.lineTo(touchX, touchY);
+                drawCanvas.drawPath(path, drawPaint);
+                path.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+    }
+
+    public void clearCanvas(){
+        drawCanvas.drawColor(Color.WHITE);
+    }
+}
--- app/src/main/java/com/example/ryan/kanakards/TraceViewer.java
+++ app/src/main/java/com/example/ryan/kanakards/TraceViewer.java
@@ -1,17 +1,49 @@
 package com.example.ryan.kanakards;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class TraceViewer extends AppCompatActivity {
+
+    private DrawView drawView;
+    private Button clearButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_trace_viewer);
 
-        //TODO something needs to go here
-        Toast.makeText(getApplicationContext(), "You made it to the Tracing Section!", Toast.LENGTH_SHORT).show();
+        drawView = findViewById(R.id.drawView);
+        clearButt = findViewById(R.id.clearButt);
+
+        drawView.setDrawingCacheEnabled(true);
+        drawView.setVisibility(View.VISIBLE);
+        drawView.setEnabled(true);
+        drawView.invalidate();
+
+        clearButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawView.clearCanvas();
+            }
+        });
+
     }
+
 }
